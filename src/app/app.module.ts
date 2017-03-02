0e19617bed603b0ec424cdadc27ac87984721705
nabeelh92 <nabeelhuq@gmail.com>
2017-03-01 21:22:43 -0800

    created dashboard component and route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,37 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }         from './app.component';
 import { HeroDetailComponent }  from './hero-detail.component';
-import { HeroesComponent }        from './heroes.component';
+import { HeroesComponent }      from './heroes.component';
 import { HeroService }          from './hero.service';
+import { DashboardComponent }   from './dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
---
+++ src/app/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-dashboard',
+  templateUrl: './dashboard.component.html',
+})
+
+export class DashboardComponent {
+
+}
